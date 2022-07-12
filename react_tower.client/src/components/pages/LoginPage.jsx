39620c2ebf4738869a974b7a8361bfb140827017
JWagstaff-Leon <wagstaffleon.joshua@gmail.com>
2022-07-12 10:25:40 -0600

    added sass

--- react_tower.client/src/components/pages/LoginPage.jsx
+++ react_tower.client/src/components/pages/LoginPage.jsx
@@ -11,16 +11,23 @@ const LoginPage = ({ doLogin }) => {
     }
 
     return ( 
-        <>
-            <form onSubmit={doLogin}>
-                <label htmlFor="login-form-email">Email</label>
-                <input type="email" name="email" id="login-form-email" placeholder="Email" className="form-control" required value={loginCreds.email} onChange={handleChange} />
-                <label htmlFor="login-form-password">Password</label>
-                <input type="password" name="password" id="login-form-password" placeholder="Password" className="form-control" required value={loginCreds.password} onChange={handleChange}/>
-                <button className="btn btn-primary mt-4">Login</button>
-            </form>
-            <Link to="/signup">Sign up for an account</Link>
-        </>
+        <div className="container">
+            <div className="row flex-column h-100 justify-content-center">
+                <div className="col-12 offset-lg-3 col-lg-6">
+                    <h1 className="text-light fs-2">Sign in to The Tower</h1>
+                    <form onSubmit={doLogin}>
+                        <label htmlFor="login-form-email" className="text-light">Email</label>
+                        <input type="email" name="email" id="login-form-email" placeholder="Email" className="form-control" required value={loginCreds.email} onChange={handleChange} />
+                        <label htmlFor="login-form-password" className="text-light mt-2">Password</label>
+                        <input type="password" name="password" id="login-form-password" placeholder="Password" className="form-control" required value={loginCreds.password} onChange={handleChange}/>
+                        <button className="btn btn-primary mt-4">Login</button>
+                    </form>
+                    <div className="mt-3">
+                        <Link to="/signup">Sign up for an account</Link>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
  
--- react_tower.client/src/components/pages/SignupPage.jsx
+++ react_tower.client/src/components/pages/SignupPage.jsx
@@ -10,14 +10,21 @@ const SignupPage = ({ doSignup }) => {
         setLoginCreds(creds);
     }
 
-    return ( 
-        <form onSubmit={doSignup}>
-            <label htmlFor="signup-form-email">Email</label>
-            <input type="email" name="email" id="signup-form-email" placeholder="Email" className="form-control" required value={loginCreds.email} onChange={handleChange} />
-            <label htmlFor="signup-form-password">Password</label>
-            <input type="password" name="password" id="signup-form-password" placeholder="Password" className="form-control" required value={loginCreds.password} onChange={handleChange}/>
-            <button className="btn btn-primary mt-4">Signup</button>
-        </form>
+    return (
+        <div className="container">
+            <div className="row flex-column h-100 justify-content-center">
+                <div className="col-12 offset-lg-3 col-lg-6">
+                    <h1 className="text-light fs-2">Sign up for The Tower</h1>
+                    <form onSubmit={doSignup}>
+                        <label htmlFor="signup-form-email" className="text-light">Email</label>
+                        <input type="email" name="email" id="signup-form-email" placeholder="Email" className="form-control" required value={loginCreds.email} onChange={handleChange} />
+                        <label htmlFor="signup-form-password" className="text-light mt-2">Password</label>
+                        <input type="password" name="password" id="signup-form-password" placeholder="Password" className="form-control" required value={loginCreds.password} onChange={handleChange}/>
+                        <button className="btn btn-primary mt-4">Sign up</button>
+                    </form>
+                </div>
+            </div>
+        </div>
      );
 }
  
