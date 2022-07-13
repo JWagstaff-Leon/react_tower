2473525b8d77523fa9b0fdf7bbd0285ab26a12cf
JWagstaff-Leon <wagstaffleon.joshua@gmail.com>
2022-07-13 12:16:13 -0600

    style

--- react_tower.client/src/components/CreateComment.jsx
+++ react_tower.client/src/components/CreateComment.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const CreateComment = ({ comment, handleChange, handleSubmit }) => {
     return ( 
         <div className="d-flex flex-column align-items-end my-4">
-            <span className="text-light">Join the conversation</span>
+            <span className="text-success lighten-10">Join the conversation</span>
             <form onSubmit={handleSubmit} className="w-100 d-flex flex-column">
                 <label className="visually-hidden" htmlFor="newComment">New Comment</label>
                 <textarea name="newComment" id="newComment" className="form-control my-2" placeholder="Tell the people" required value={comment} onChange={handleChange}></textarea>
