97987f83fbfba8c8291c10d368b64474a4d3c0ec
JWagstaff-Leon <wagstaffleon.joshua@gmail.com>
2022-07-14 15:32:38 -0600

    async password hashing

--- react_tower/server/services/AccountService.js
+++ react_tower/server/services/AccountService.js
@@ -1,13 +1,14 @@
 import { BadRequest, Forbidden } from '@bcwdev/auth0provider/lib/Errors.js';
 import { dbContext } from '../db/DbContext'
+import { logger } from '../utils/Logger.js';
 
 const bcrypt = require("bcrypt");
 
 // Private Methods
 
-function hashPassword(password)
+async function hashPassword(password)
 {
-    return bcrypt.hashSync(password, 15);
+    return await bcrypt.hash(password, 15);
 }
 
 
@@ -18,8 +19,7 @@ function hashPassword(password)
 function sanitizeBody(body) {
     const writable = {
         name: body.name,
-        picture: body.picture,
-        password: hashPassword(body.password)
+        picture: body.picture
     }
     return writable
 }
@@ -27,18 +27,18 @@ function sanitizeBody(body) {
 class AccountService {
     async createAccount({ email, password })
     {
-        const data = { email, password };
+        const data = { email, password: null, picture: "https://thiscatdoesnotexist.com/", name: email };
         if(await dbContext.Account.findOne({ email }))
         {
             throw new Forbidden("Account with that email already exists.");
         }
+        data.password = await hashPassword(password);
 
-        data.password = hashPassword(data.password);
-        data.name = data.email;
-        data.picture = "https://thiscatdoesnotexist.com/";
-
-        const newUser = await dbContext.Account.create(data);
-        return newUser;
+        if(password)
+        {
+            const newUser = await dbContext.Account.create(data);
+            return newUser;
+        }
     }
 
     async getAccount({ email, password }) {
@@ -48,7 +48,7 @@ class AccountService {
             throw new BadRequest("Account does not exist.");
         }
 
-        const validPassword = bcrypt.compareSync(password, account.password);
+        const validPassword = await bcrypt.compare(password, account.password);
         if(!validPassword)
         {
             throw new BadRequest("Incorrect password");
